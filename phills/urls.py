897b1379de47a357b089aa7b3dbc027ea32eb29c
patotec <ewaen.patrick300@gmail.com>
2024-04-06 08:24:02 +0100

    first

--- phills/urls.py
+++ phills/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('', include('index.urls', namespace='index')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
